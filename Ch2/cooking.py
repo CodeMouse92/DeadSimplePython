5a653e4d4fe64821f63a092b8127c7f7fe129458
Jason C. McDonald <jcmcdonald@mousepawmedia.com>
2022-05-15 21:44:20 -0500

    Chapter 2

---
+++ Ch2/cooking.py
@@ -0,0 +1,6 @@
+# pylint: disable=missing-docstring
+
+def cooking():  # pylint: disable=missing-docstring
+    ham = True    # noqa F841
+    print(eggs)   # noqa F821, F841
+    return order  # noqa
